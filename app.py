b5f24e416a8f8c5c285045d3c699e20407e2a5dd
EC2 Default User <ec2-user@ip-172-31-8-250.us-east-2.compute.internal>
2022-04-20 03:01:48 +0000

    added app.py

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def my_form():
 if __name__=='__main__':
     #app.run(host='0.0.0.0', port=5000, debug=True,ssl_context=('cert.pem', 'key.pem'))#'adhoc'
     #serve(app, host='0.0.0.0', port=5000,url_scheme='https')
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
